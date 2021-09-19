fbb9d98198c0a2ed5b1bc705cd328208d82f162e
Bailey Pardee <baileypardee@gmail.com>
2021-09-19 16:27:11 -0400

    button with hover

--- invisi-button/src/InvisiButton.js
+++ invisi-button/src/InvisiButton.js
@@ -3,35 +3,91 @@ import { html, css, LitElement } from 'lit';
 export class InvisiButton extends LitElement {
   static get styles() {
     return css`
-      :host {
-        display: block;
-        padding: 25px;
-        color: var(--invisi-button-text-color, #000);
+    :host {
+      display inline-block;
+      --invisi-button-color: var(
+        --simple-colors-default-theme-accent-1,
+        white
+        );
+        --invisi-button-outline: var(
+          --simple-colors-default-theme-accent-12,
+          black
+        );
+        --invisi-button-bg-color-is-user-selected: var(
+          --simple-colors-default-theme-accent-10,
+          darkgreen
+        );
+        --invisi-button-bg-color: var(
+          --simple-colors-default-theme-accent-7,
+          black
+        );
+        margin: 60px 0 0;
       }
-    `;
-  }
+      :hover{
+        background-color: transparent;
+        color: black;
+        border: 2px solid black; 
+      }
+      :is-user-selected {
+        background-color: var(--invisi-button-bg-color-is-user-selected);
+        outline: 1px solid var(--invisi-button-outline);
+      }
+      a {
+        display: block;
+        color: var(--invisi-button-color);
+        background-color: var(--invisi-button-bg-color);
+        transition: background 0.5s linear, border 0.3s linear,
+          border-radius 0.3s linear, box-shadow 0.3s linear;
+        text-decoration: none;
+        font-size: 18px;
+        border-radius: 5px 5px 5px 5px;
+        box-shadow: 0 6px 26px 0 rgba(0, 0, 0, 0.16);
+        padding: 15px 15px;
+        font-family: Sans-serif;
+        font-weight: 540;
+        width: 150px;
+
+      }
+      a span {
+        display: flex;
+        justify-content: center;
+      }
+      
+  `;
+  
+}
 
   static get properties() {
     return {
-      title: { type: String },
-      counter: { type: Number },
+      link: { type: String },
+      editMode: { type: Boolean },
+      title: { type: String }
     };
   }
 
   constructor() {
     super();
-    this.title = 'Hey there';
-    this.counter = 5;
+    this.link = "#";
+    this.title = null;
+    this.accentColor = "black";
+    if (this.querySelector("a")) {
+      this.link = this.querySelector("a").getAttribute("href");
+      this.title = this.querySelector("a").innerText;
+      this.innerHTML = null;
+    }
   }
 
-  __increment() {
-    this.counter += 1;
+  _clickCard(e) {
+    if (this.editMode) {
+      e.preventDefault();
+      e.stopPropagation();
+      e.stopImmediatePropagation();
+    }
   }
 
   render() {
-    return html`
-      <h2>${this.title} Nr. ${this.counter}!</h2>
-      <button @click=${this.__increment}>increment</button>
+    return html` <a href="${this.link}" role="button" part="simple-cta-link" @click="${this._clickCard}" ?contenteditable="${this.editMode}">
+      <span><span id="title">${this.title}</span><slot></slot></span></a>
     `;
   }
 }
